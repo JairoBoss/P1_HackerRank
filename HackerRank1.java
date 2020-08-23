7e43e6c5ed9f53fafaa5085a765465831b00479e
JairoBoss <61020858+JairoBoss@users.noreply.github.com>
2020-08-23 18:49:48 -0500

    1

--- HackerRank1.java
+++ HackerRank1.java
@@ -1,20 +1,7 @@
-package hackerrank1java1darray;
-
-import java.util.Scanner;
-public class HackerRank1Java1DArray {
-
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        int tamaño;
-        tamaño=sc.nextInt();
-        int array[]=new int [tamaño];
-        for(int x=0;x<array.length;x++){
-            array[x]=sc.nextInt();
-        
-    }
-        for (int i = 0; i < array.length; i++) {
-            System.out.println(array[i]);
+    static void printLinkedList(SinglyLinkedListNode head) {
+        SinglyLinkedListNode aux = head;
+        while(aux!=null){
+            System.out.println(aux.data);
+            aux=aux.next;
         }
     }
-    
-}
